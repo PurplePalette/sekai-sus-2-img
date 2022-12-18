bc42d659ae83521c8ed9c1991d27985445208fae
k0tayan <kotayan0415@outlook.jp>
2022-12-18 12:33:14 +0900

    横長画像の高さを4小節に変更

--- public/js/landscape.js
+++ public/js/landscape.js
@@ -1,21 +1,22 @@
 window.onload = () => {
     const canvas = document.getElementById('chart-landscape');
-    canvas.height = pixelsPerBeat * 8;
+    const chartHeight = pixelsPerBeat * 16;
+    canvas.height = chartHeight;
     const ctx = canvas.getContext("2d");
     const chartImage = new Image();
     chartImage.src = "data:image/png;base64," + pngString;
     chartImage.onload = () => {
         const width = chartImage.width;
-        const height = chartImage.height;
-        const m = Math.floor(height / (pixelsPerBeat * 8));
+        const height = chartImage.height; // 縦長の画像の高さ
+        const m = Math.floor(height / chartHeight);
         let totalLeft = 0;
         canvas.width = width * (m + 1);
         for (let i = 0; i < m; i++) {
-            const top = height - (i + 1) * pixelsPerBeat * 8;
+            const top = height - (i + 1) * chartHeight;
             const left = totalLeft;
             totalLeft += width;
-            ctx.drawImage(chartImage, 0, top, width, pixelsPerBeat * 8, left, 0, width, pixelsPerBeat * 8);
+            ctx.drawImage(chartImage, 0, top, width, chartHeight, left, 0, width, chartHeight);
         }
-        ctx.drawImage(chartImage, 0, 0, width, height % (pixelsPerBeat * 8), totalLeft, pixelsPerBeat * 8 - height % (pixelsPerBeat * 8), width, height % (pixelsPerBeat * 8));
+        ctx.drawImage(chartImage, 0, 0, width, height % (chartHeight), totalLeft, chartHeight - height % chartHeight, width, height % chartHeight);
     }
 }
